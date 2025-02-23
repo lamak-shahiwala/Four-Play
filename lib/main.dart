2a918e7e705a9d54f32d6526a6efb7f824ff5f78
lamak-shahiwala <shahiwalalamak@gmail.com>
2025-02-23 13:37:57 +0530

    Initial Commit.

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:four_play/screens/dashboard.dart';
+
+void main() {
+  runApp(MaterialApp(home: Dashboard(),));
+}
+
---
+++ lib/utilities/text/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const blue_dashboard_button_text = TextStyle(
+color: Color.fromRGBO(255, 255, 255, 1),
+fontSize: 24,
+fontFamily: 'Sofia_Sans',
+fontWeight: FontWeight.w400,
+);
