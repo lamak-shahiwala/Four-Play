4ccd903410d370288ec4952e72ad4b38961f5c8d
lamak-shahiwala <shahiwalalamak@gmail.com>
2025-02-24 19:37:19 +0530

    removed flutter switch package

--- lib/utilities/switch/settings_sound_switch.dart
+++ lib/utilities/switch/settings_sound_switch.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_switch/flutter_switch.dart';
 
 class SettingsSoundSwitch extends StatefulWidget {
   const SettingsSoundSwitch({super.key});
