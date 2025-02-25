debf803d3c7089df5972df6bb63c15ecac54128b
Aaquif Qureshi <aaquifkureshi04.ak@gmail.com>
2025-02-25 18:05:43 +0530

    Sound switch bug solved
    Alignment issue of the sound switch Solved

--- lib/utilities/switch/settings_sound_switch.dart
+++ lib/utilities/switch/settings_sound_switch.dart
@@ -8,20 +8,27 @@ class SettingsSoundSwitch extends StatefulWidget {
 }
 
 class _SettingsSoundSwitchState extends State<SettingsSoundSwitch> {
-
-  bool switch_value = true;
+  bool switchValue = true;
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: 0.55,
-      child: Switch(value: switch_value, onChanged: (value){
-        setState(() {
-          switch_value = value;
-        });
-      },
-        activeColor: Colors.black,
-        inactiveThumbColor: Colors.black,
+    return Align(
+      alignment: Alignment.centerRight, // Ensures right alignment
+      child: SizedBox(
+        width: 30, // Controls width to align properly
+        child: Transform.scale(
+          scale: 0.55, // Adjusted scale for better visibility
+          child: Switch(
+            value: switchValue,
+            onChanged: (value) {
+              setState(() {
+                switchValue = value;
+              });
+            },
+            activeColor: Colors.black,
+            inactiveThumbColor: Colors.black,
+          ),
+        ),
       ),
     );
   }
